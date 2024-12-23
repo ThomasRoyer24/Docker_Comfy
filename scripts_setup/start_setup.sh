12ecd122be9db8f14ff60439bd9b451e83f629e6
ThomasRoyer24 <thomas.royer24@gmail.com>
2024-12-23 17:57:45 +0100

    update start_setup.sh to include output directory for ComfyUI

--- scripts_setup/start_setup.sh
+++ scripts_setup/start_setup.sh
@@ -1,4 +1,4 @@
-python3.10 -u main.py --listen --port 7860 &
+python3.10 -u main.py --listen --port 7860 & 
 
 PID_COMFY=$!
 echo "ComfyUI démarré avec PID: $PID_COMFY"
@@ -14,4 +14,4 @@ python3.10 custom_nodes/ComfyUI-Manager/cm-cli.py fix all
 kill $PID_COMFY
 echo "ComfyUI (PID: $PID_COMFY) a été arrêté"
 
-python3.10 -u main.py --listen --port 7860 &
+python3.10 -u main.py --listen --port 7860 --output-directory ../output &
