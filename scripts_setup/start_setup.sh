933c4b41764f183c07753644ded958168aa1b755
ThomasRoyer24 <thomas.royer24@gmail.com>
2025-01-23 18:38:24 +0100

    reduce sleep duration in start_setup.sh from 30 to 20 seconds

--- scripts_setup/start_setup.sh
+++ scripts_setup/start_setup.sh
@@ -1,10 +1,12 @@
+# Il faut surement lancer 2 fois le script pour paler au bug 'load image'
+
 python3.10 -u main.py --listen --port 7860 & 
 
 PID_COMFY=$!
 echo "ComfyUI démarré avec PID: $PID_COMFY"
 
 # Attendre 30 secondes
-sleep 30
+sleep 20
 
 python3.10 custom_nodes/ComfyUI-Manager/cm-cli.py update all
 
